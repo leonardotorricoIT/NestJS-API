91136cb3d4b6dcffe58e19218631b33ce193bb46
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-08-13 12:42:46 -0400

    feat: attempt to connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import databaseConfig from './database/database.config';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    DatabaseModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+        logging: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
