56a018b154ece12401d344ce7a7acbaced01e3a6
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-08-14 09:45:57 -0400

    feat: set modules into the proyect

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import databaseConfig from './database/database.config';
 import { ProductsModule } from './products/products.module';
+import { ArtistsModule } from './artists/artists.module';
+import { AlbumsModule } from './albums/albums.module';
+import { GenresModule } from './genres/genres.module';
+import { SongsModule } from './songs/songs.module';
+import { ArtistService } from './artist/artist.service';
 
 @Module({
   imports: [
@@ -13,6 +18,11 @@ import { ProductsModule } from './products/products.module';
     }),
     DatabaseModule,
     ProductsModule,
+    ArtistsModule,
+    AlbumsModule,
+    GenresModule,
+    SongsModule,
   ],
+  providers: [ArtistService],
 })
 export class AppModule {}
